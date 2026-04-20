35c637ba089ee258f49159214dfdfa1f7a6bd6dc
Ricardo Díaz <ricardotk002@gmail.com>
2026-04-20 14:17:38 -0400

    Fix broken migration

--- server/src/schema/migrations/1777000000000-AddMlTrainingOptInToUser.ts
+++ server/src/schema/migrations/1777000000000-AddMlTrainingOptInToUser.ts
@@ -1,7 +1,7 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await sql`ALTER TABLE "user" ADD COLUMN "mlTrainingOptIn" boolean NOT NULL DEFAULT false;`.execute(db);
+  await sql`ALTER TABLE "user" ADD COLUMN IF NOT EXISTS "mlTrainingOptIn" boolean NOT NULL DEFAULT false;`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
